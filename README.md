3f40e6602fc1f4e5aa016e21d955eb35b1f508f0
James Chambers <>
2019-05-14 21:47:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Next Portfolio
+
+Creating a next js server side rendered react Portfolio project.
+
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+
+let yolo = "yolor!!!";
+
+const Index = () => <div>Welcome to the {yolo}</div>;
+
+export default Index;
