c2377a3e42d5e09641513630fb61a3eb207a1abe
James Chambers <>
2019-06-25 18:55:36 -0700

    updated with cookie

--- auth/server.js
+++ auth/server.js
@@ -1,11 +1,24 @@
 const next = require('next');
 const express = require('express');
-
+const axios = require('axios');
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 
+const AUTH_USER_TYPE = "authenticated";
+
 const handle = app.getRequestHandler();//before implementing auth
+// https://jsonplaceholder.typicode.com/users - to generate a user
+const authenticate = async (email, password) => {
+  const {
+    data
+  } = await axios.get('https://jsonplaceholder.typicode.com/users')
+  return data.find(user => {
+    if(user.email === email && user.username === password){
+      return user;
+    }
+  })
+}
 
 app.prepare().then(()=> {
   const server = express();
@@ -13,13 +26,25 @@ app.prepare().then(()=> {
   // express middleware
   server.use(express.json());
 
-  server.post('/api/login',(req, res) => {
+  server.post('/api/login', async (req, res) => {
     const { email, password } = req.body;
-    res.json({
-      email, 
-      password, 
-      success: true
-    });
+    const user = await authenticate(email, password);
+    if (!user){
+      return res.status(403).send("Invalid email or password")
+    }
+    const userData = {
+      name: user.name,
+      email: user.email,
+      type: AUTH_USER_TYPE
+    }
+    res.cookie();
+    res.json(userData);
+    // res.json({
+    //   email, 
+    //   password, 
+    //   success: true
+    // });
+
   });
 
   server.get("*",(req, res) => {
